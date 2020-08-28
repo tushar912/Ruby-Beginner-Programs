970b129c0ce6b171d1fd85aa54601a9c230902d8
Tushar912 <tushar.912u@gmail.com>
2020-08-28 18:23:55 +0530

    first program

---
+++ analyzer.rb
@@ -0,0 +1,9 @@
+print "enter your first name"
+fisrt_name= gets.chomp
+print"enter lastname"
+last_name = gets.chomp
+name = fisrt_name+" "+last_name
+
+puts "your full name is #{name}"
+puts "your name is #{name.length} long"
+puts "your name is #{name.reverse} in reverse"
